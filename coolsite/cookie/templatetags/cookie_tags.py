a75a7098ce23942417b90f08e8f5acc968c45d99
niki <fdtdhth@mail.ru>
2023-05-08 15:38:09 +0300

    usertags

--- coolsite/cookie/templatetags/cookie_tags.py
+++ coolsite/cookie/templatetags/cookie_tags.py
@@ -3,5 +3,13 @@ from cookie.models import *
 
 register = template.Library()
 @register.simple_tag(name='appealcats')
-def get_categories():
-    return Category.objects.all()
+def get_categories(fileter=None):
+    if not fileter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=fileter)
+
+@register.inclusion_tag('cookie/list_categories.html')
+def show_categories():
+    cats = Category.objects.all()
+    return {"cats": cats}
