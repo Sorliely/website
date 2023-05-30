57ae2e41e73edc7f9bd4629b689c81e188c1ee65
niki <fdtdhth@mail.ru>
2023-05-30 22:36:50 +0300

    register interval

--- coolsite/cookie/models.py
+++ coolsite/cookie/models.py
@@ -8,20 +8,20 @@ class Cookie(models.Model):
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    time_create = models.DateTimeField(auto_now_add=True) #время создание записи
-    time_update = models.DateTimeField(auto_now=True) #время обновления
+    time_create = models.DateTimeField(auto_now_add=True)  # время создание записи
+    time_update = models.DateTimeField(auto_now=True)  # время обновления
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT,)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, )
 
     def __str__(self):
         """выводит название записей в бд """
         return self.title
 
-
     class Meta:
         verbose_name = 'Популярные печенья'
         verbose_name_plural = 'Популярные печенья'
         ordering = ['time_create', 'title']
+
 
 class Recipe(models.Model):
     """БД рецептов"""
@@ -39,6 +39,7 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
+
 class Category(models.Model):
     """Эта БД связывает все остальные"""
     name = models.CharField(max_length=100, db_index=True)
@@ -50,5 +51,5 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
 
-#login Nikki
+# login Nikki
 # password superuser  gOGmvzkmtL3s
--- coolsite/cookie/views.py
+++ coolsite/cookie/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse, HttpResponseNotFound,Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .forms import *
 from .models import *
+from .utils import *
 
 
-class CookieHome(ListView):
+class CookieHome(DataMixin, ListView):
     model = Cookie
     template_name = 'cookie/index.html'
     context_object_name = 'posts'
@@ -13,13 +18,12 @@ class CookieHome(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        context['cat_selected'] = 0
-
-        return context
+        c_def = self.get_user_context(title="Главная странциа")
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Cookie.objects.filter(is_published=True)
+
 
 # def index(request):
 #     """главная страница"""
@@ -30,13 +34,16 @@ class CookieHome(ListView):
 #                }
 #     return render(request, 'cookie/index.html', context=context)
 
-def history(request,):
+def history(request, ):
     recipe = Recipe.objects.all()
     return render(request, 'cookie/history.html', {'recipe': recipe})
+
 
 def about(request):
     return render(request, "cookie/about.html")
 
+
+@login_required
 def addpage(request):
     """функция авторизации
     выполняет фунуцию приема данных и если данные ошибочные возвращает
@@ -44,16 +51,18 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
-            #print(form.cleaned_data)
-            #добавление новой записи в бд
+            # print(form.cleaned_data)
+            # добавление новой записи в бд
             form.save()
             return redirect('home')
     else:
         form = AddPostForm
     return render(request, 'cookie/addpage.html', {'form': form, 'title': 'Добавление статьи'})
 
+
 def login(request):
-    return HttpResponseNotFound("Автаризация")
+    return HttpResponse("Автаризация")
+
 
 # def show_post(request, post_id):
 #     return HttpResponseNotFound(f'Отображает статьи с id = {post_id}')
@@ -61,6 +70,7 @@ def login(request):
 def pageNotFound(request, exception):
     """отоброжение ошибки 404"""
     return HttpResponseNotFound("<h1>Страница не существует</h1>")
+
 
 def show_post(request, post_slug):
     post = get_object_or_404(Cookie, slug=post_slug)
@@ -71,6 +81,7 @@ def show_post(request, post_slug):
     }
 
     return render(request, 'cookie/post.html', context=context)
+
 
 def show_category(request, cat_id):
     """категориии хз зачем"""
@@ -83,3 +94,14 @@ def show_category(request, cat_id):
     if len(posts) == 0:
         raise Http404()
     return render(request, 'cookie/index.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = UserCreationForm
+    template_name = 'cookie/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, objects_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(titel='регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
